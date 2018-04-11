61a121d261ae225b3de8bfb2e7f9d3f9154525b9
Arsalan Chandwani <arsalanchandwani@gmail.com>
2018-04-11 15:28:33 -0500

    Created Activities for UI

---
+++ app/src/main/java/com/cse3310/cse3310_group_one_project/CatererRemoveResourcesActivity.java
@@ -0,0 +1,16 @@
+package com.cse3310.cse3310_group_one_project;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Arsalan on 4/11/2018.
+ */
+
+public class CatererRemoveResourcesActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState)  {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_caterer_remove_resources);
+    }
+}
--- app/src/main/java/com/cse3310/cse3310_group_one_project/MainActivity.java
+++ app/src/main/java/com/cse3310/cse3310_group_one_project/MainActivity.java
@@ -1,7 +1,10 @@
 package com.cse3310.cse3310_group_one_project;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button register_button = (Button) findViewById(R.id.Register);
+        register_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                open_Register();
+            }
+        });
+    }
+    public void open_Register(){
+        Intent intent_register = new Intent(this,RegisterActivity.class);
+        startActivity(intent_register);
     }
 }
