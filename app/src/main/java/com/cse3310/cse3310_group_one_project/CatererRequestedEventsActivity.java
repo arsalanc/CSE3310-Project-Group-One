ac3a447bb6494f5039b429320be900fc75630444
Arsalan Chandwani <arsalanchandwani@gmail.com>
2018-04-12 16:28:59 -0500

    UI id's and small changes

--- app/src/main/java/com/cse3310/cse3310_group_one_project/CatererRequestedEventsActivity.java
+++ app/src/main/java/com/cse3310/cse3310_group_one_project/CatererRequestedEventsActivity.java
@@ -1,7 +1,10 @@
 package com.cse3310.cse3310_group_one_project;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by Arsalan on 4/11/2018.
@@ -12,5 +15,30 @@ public class CatererRequestedEventsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState)  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_caterer_requested_events);
+        //TODO:view_details, reserve and deny buttons
+        Button view_details = (Button) findViewById(R.id.requested_event_details);
+        Button reserve = (Button) findViewById(R.id.reserve);
+        Button deny = (Button) findViewById(R.id.deny);
+        Button back = (Button) findViewById(R.id.requested_events_back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                requested_back();
+            }
+        });
+    }
+
+    public void viewDetails(){
+
+    }
+    public void reserve(){
+
+    }
+    public void deny(){
+
+    }
+    public void requested_back(){
+        Intent intent_requestBack = new Intent(this,CatererHomepageActivity.class);
+        startActivity(intent_requestBack);
     }
 }
