df3b9830e66332c88521d90c50c7e9d3ab5a95f3
Sol-Weintraub <miguelfr08@gmail.com>
2018-04-26 21:11:50 -0500

    add resources almost done. Need to add entries to the database.

--- app/src/main/java/com/cse3310/cse3310_group_one_project/Activities/CatererAddResources.java
+++ app/src/main/java/com/cse3310/cse3310_group_one_project/Activities/CatererAddResources.java
@@ -12,6 +12,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.cse3310.cse3310_group_one_project.Models.DBManager;
+import com.cse3310.cse3310_group_one_project.Models.Event;
 import com.cse3310.cse3310_group_one_project.Models.Resource;
 import com.cse3310.cse3310_group_one_project.Models.User;
 import com.cse3310.cse3310_group_one_project.R;
@@ -24,9 +25,9 @@ import com.cse3310.cse3310_group_one_project.R;
  * Im adding a simple test comment here
  */
 public class CatererAddResources extends AppCompatActivity {
-    Spinner resource_type;
-    TextView resource_amount;
-    Button btn_add, btn_sub;
+    Spinner food_spinner,drink_spinner,ent_spinner;
+    //TextView resource_amount;
+    //Button btn_add, btn_sub;
     DBManager db;
     int amount = 0;
     @Override
@@ -36,75 +37,105 @@ public class CatererAddResources extends AppCompatActivity {
         //TODO: submit button functionality
         Button add_resources_submit=findViewById(R.id.add_resources_submit_a);
         Button add_resources_back=findViewById(R.id.add_resources_back_a);
-        btn_add = (Button)findViewById(R.id.btn_add_resources_a);
-        btn_sub = (Button)findViewById(R.id.btn_sub_resources_a);
-        resource_amount = findViewById(R.id.resource_amount);
-        resource_type = findViewById(R.id.resource_type);
-        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(CatererAddResources.this,
-                R.layout.spinner_item2,getResources().getStringArray(R.array.Resource_Types));
-        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        resource_type.setAdapter(myAdapter);
+        food_spinner = findViewById(R.id.foodSpinner);
+        drink_spinner = findViewById(R.id.drinkSpinner);
+        ent_spinner = findViewById(R.id.entSpinner);
         db=new DBManager(this);
-        int event_id = (int) getIntent().getSerializableExtra("EVENT_ID");
-        String type = resource_type.getSelectedItem().toString();
-        //amount = db.retrieveAmountResources(event_id,type);
-        resource_amount.setText("Amount: "+amount);
-        add_resources_back.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                addResourcesBack();
-            }
-        });
-        add_resources_submit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                addResourcesSubmit(db);
-            }
-        });
-        btn_add.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                amount++;
-                resource_amount.setText("Amount: "+amount);
-                resource_amount.setTextColor(Color.WHITE);
-            }
-        });
-        btn_sub.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(amount<=0)
-                    Toast.makeText(CatererAddResources.this, "You can't do that", Toast.LENGTH_LONG).show();
-                else {
-                    amount--;
-                    resource_amount.setText("Amount: " + amount);
-                    resource_amount.setTextColor(Color.WHITE);
-                }
-            }
-        });
+        //int event_id = (int) getIntent().getSerializableExtra("EVENT_ID");
+        Event e = (Event) getIntent().getSerializableExtra("EVENT");
+        String mealType = e.getMeal_type();
+        String drinkType = e.getDrink_venue();
+        //FOOD
+        if(mealType.equalsIgnoreCase("american")){
+            ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(CatererAddResources.this,
+                    R.layout.spinner_item2,getResources().getStringArray(R.array.food_american));
+            myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            food_spinner.setAdapter(myAdapter);
+        }
+        else if(mealType.equalsIgnoreCase("chinese")){
+            ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(CatererAddResources.this,
+                    R.layout.spinner_item2,getResources().getStringArray(R.array.food_chinese));
+            myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            food_spinner.setAdapter(myAdapter);
+        }
+        else if(mealType.equalsIgnoreCase("french")){
+            ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(CatererAddResources.this,
+                    R.layout.spinner_item2,getResources().getStringArray(R.array.food_french));
+            myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            food_spinner.setAdapter(myAdapter);
+        }
+        else if(mealType.equalsIgnoreCase("greek")){
+            ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(CatererAddResources.this,
+                    R.layout.spinner_item2,getResources().getStringArray(R.array.food_greek));
+            myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            food_spinner.setAdapter(myAdapter);
+        }
+        else if(mealType.equalsIgnoreCase("indian")){
+            ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(CatererAddResources.this,
+                    R.layout.spinner_item2,getResources().getStringArray(R.array.food_indian));
+            myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            food_spinner.setAdapter(myAdapter);
+        }
+        else if(mealType.equalsIgnoreCase("italian")){
+            ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(CatererAddResources.this,
+                    R.layout.spinner_item2,getResources().getStringArray(R.array.food_italian));
+            myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            food_spinner.setAdapter(myAdapter);
+        }
+        else if(mealType.equalsIgnoreCase("japanese")){
+            ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(CatererAddResources.this,
+                    R.layout.spinner_item2,getResources().getStringArray(R.array.food_japanese));
+            myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            food_spinner.setAdapter(myAdapter);
+        }
+        else{
+            ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(CatererAddResources.this,
+                    R.layout.spinner_item2,getResources().getStringArray(R.array.food_mexican));
+            myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            food_spinner.setAdapter(myAdapter);
+        }
+        //DRINK
+        if(drinkType.equalsIgnoreCase("alcoholic")){
+            ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(CatererAddResources.this,
+                    R.layout.spinner_item2,getResources().getStringArray(R.array.drinks_alcoholic));
+            myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            drink_spinner.setAdapter(myAdapter);
+        }
+        else{
+            ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(CatererAddResources.this,
+                    R.layout.spinner_item2,getResources().getStringArray(R.array.drinks_standard));
+            myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            drink_spinner.setAdapter(myAdapter);
+        }
+        //ENT
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(CatererAddResources.this,
+                R.layout.spinner_item2,getResources().getStringArray(R.array.ent_items));
+        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        ent_spinner.setAdapter(myAdapter);
     }
 
     public void addResourcesSubmit(DBManager db){
         int event_id = (int) getIntent().getSerializableExtra("EVENT_ID");
-        String type = resource_type.getSelectedItem().toString();
+        String foodChoice = food_spinner.getSelectedItem().toString();
+        String drinkChoice = drink_spinner.getSelectedItem().toString();
+        String entChoice = ent_spinner.getSelectedItem().toString();
         int new_amount;
-        if (type.equalsIgnoreCase("Meal") || type.equalsIgnoreCase("Drink") || type.equalsIgnoreCase("Entertainment Item")){
-            Resource r = new Resource(amount,type,event_id);
-            int old_amount = db.retrieveAmountResources(event_id,type);
-            if(old_amount==0)
-                db.addResources(r);
-            else {
-                new_amount=old_amount + amount;
-                db.updateResources(event_id, new_amount,type);
-            }
-            Intent intent_back = new Intent(this,CatererEditEvent.class);
-            intent_back.putExtra("EVENT_ID",event_id);
-            User user = (User) getIntent().getSerializableExtra("USER");
-            intent_back.putExtra("PREVIOUS_PAGE", (Class) getIntent().getSerializableExtra("PREVIOUS_PAGE"));
-            intent_back.putExtra("USER", user);
-            startActivity(intent_back);
+        if ((!foodChoice.equalsIgnoreCase("american food") || !foodChoice.equalsIgnoreCase("chinese food") || !foodChoice.equalsIgnoreCase("indian food") || !foodChoice.equalsIgnoreCase("italian food")
+        || !foodChoice.equalsIgnoreCase("japanese food") || !foodChoice.equalsIgnoreCase("greek food") || !foodChoice.equalsIgnoreCase("french food") || !foodChoice.equalsIgnoreCase("mexican food"))
+                && !drinkChoice.equalsIgnoreCase("Drink") && !entChoice.equalsIgnoreCase("Entertainment Item")){
+
+            // ADD entries to the database
+
         }else{
-            Toast.makeText(this, "Please choose a resource type", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "A resource was not chosen.", Toast.LENGTH_SHORT).show();
         }
+
+        Intent intent_back = new Intent(this,CatererEditEvent.class);
+        intent_back.putExtra("EVENT_ID",event_id);
+        User user = (User) getIntent().getSerializableExtra("USER");
+        intent_back.putExtra("PREVIOUS_PAGE", (Class) getIntent().getSerializableExtra("PREVIOUS_PAGE"));
+        intent_back.putExtra("USER", user);
+        startActivity(intent_back);
     }
     public void addResourcesBack(){
         Intent intent_back = new Intent(this,CatererEditEvent.class);
